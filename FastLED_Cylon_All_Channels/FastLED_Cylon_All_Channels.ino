06e34fb6f98dd70d201e90856d1a7081a1da603d
Ian Cole <ian.cole@gmail.com>
2020-12-26 08:43:16 -0500

    Add OLED Support

--- FastLED_Cylon_All_Channels/FastLED_Cylon_All_Channels.ino
+++ FastLED_Cylon_All_Channels/FastLED_Cylon_All_Channels.ino
@@ -4,6 +4,20 @@
 #define USE_WS2812SERIAL
 #include <FastLED.h>
 
+//oled screen includes
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+
+
 // How many leds in your strip?
 #define NUM_LEDS1 10
 #define NUM_LEDS2 64
@@ -36,7 +50,30 @@ void setup() {
   LEDS.addLeds<WS2812SERIAL,33,RGB>(leds4,NUM_LEDS4);
   
 	LEDS.setBrightness(84);
- Serial.begin(115200);
+  Serial.begin(115200);
+
+    //display init
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  Serial.println("OLED begun");
+  
+  // Clear the buffer
+  display.clearDisplay();
+
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println(__FILENAME__);
+  display.println(__DATE__);
+  display.println(__TIME__);
+  display.println("Initializing...");
+  display.display(); // actually display all of the above
+  delay(1000);
+  
 }
 
 void fadeall() {
